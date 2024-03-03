e57c85b70c6e9b4e14309b5bf154c63ca7e3e698
bitflame <monazzam@protonmail.com>
2024-03-03 09:48:13 -0500

    Needs fixing

--- src/App.java
+++ src/App.java
@@ -133,26 +133,91 @@ public class App {
         return "";
     }
 
+    public static String minWindowLeetCode(String s, String t) {
+        if (t == null || s == null)
+            return "";
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+        int haveCounter = 0, needCounter = t.length(), index = 0, minLen = s.length(), tempValue = 0, startOfString = 0,
+                sLength = s.length();
+        char currentChar;
+        int[] res = new int[2];
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+        for (int i = 0; i < tChar.length; i++) {
+            currentChar = tChar[i];
+            if (tMap.containsKey(currentChar)) {
+                tempValue = tMap.get(currentChar);
+                tMap.put(currentChar, tempValue++);
+            } else
+                tMap.put(currentChar, 1);
+            sMap.put(currentChar, 0);
+        }
+        // for (int i = 0; i < sChar.length; i++) {
+        // currentChar = sChar[i];
+        // if (sMap.containsKey(currentChar)) {
+        // tempValue = sMap.get(currentChar);
+        // sMap.put(currentChar, ++tempValue);
+        // } else
+        // sMap.put(currentChar, 1);
+
+        // }
+        while (index < sLength) {
+            currentChar = sChar[index];
+            if (tMap.get(currentChar) != null) {
+                tempValue = sMap.get(currentChar);
+                if (tempValue < tMap.get(currentChar))
+                    haveCounter++;
+                sMap.put(currentChar, ++tempValue);
+            }
+
+            if (haveCounter == needCounter) {
+                if (minLen > index - startOfString) {
+                    minLen = index - startOfString;
+                    res[0] = startOfString;
+                    res[1] = ++index;
+                }
+                currentChar = sChar[startOfString];
+                tempValue = sMap.get(currentChar);
+                sMap.put(currentChar, --tempValue);
+                if (tempValue < tMap.get(currentChar))
+                    haveCounter--;
+                startOfString++;
+                if (startOfString < sLength) {
+                    currentChar = sChar[startOfString];
+                }
+                while (tMap.get(currentChar) == null && startOfString < sLength - 1) {
+                    startOfString++;
+                    currentChar = sChar[startOfString];
+                }
+            }
+            index++;
+        }
+        return s.substring(res[0], res[1]);
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         String s = "ADOBECODEBANC";
         String t = "ABC";
-        System.out.println(minimumWindow(s, t));
+        System.out.println("Output of s = \"ADOBECODEBANC\" and t=\"ABC\" :" + "\"" + minWindowLeetCode(s, t) + "\"");
         s = "a";
         t = "a";
-        System.out.println(minimumWindow(s, t));
+        System.out.println("Output of s = \"a\" and t=\"a\" :" + "\"" + "\"" + minWindowLeetCode(s, t) + "\"");
         s = "aa";
-        System.out.println(minimumWindow(s, t));
+        System.out.println("Output of s = \"a\" and t=\"a\" :" + "\"" + "\"" + minWindowLeetCode(s, t) + "\"");
         t = "aa";
         s = "a";
-        System.out.println("Output of s = \"a\" and t=\"aa\" :" + minWindow(s, t));
+        System.out.println(
+                "Output of s = \"a\" and t=\"aa\" Expecting: \"\" Getting:" + "\"" + minWindowLeetCode(s, t) + "\"");
         t = "b";
         s = "a";
-        System.out.println("Output of s = \"a\" and t=\"b\" :" + minWindow(s, t));
+        System.out.println(
+                "Output of s = \"a\" and t=\"b\" Expecting: \"\" Getting:" + "\"" + minWindowLeetCode(s, t) + "\"");
         t = "a";
         s = "ab";
-        System.out.println("Output of s = \"ab\" and t=\"a\" Expecting \"a\" Getting:" + minWindow(s, t));
+        System.out.println("Output of s = \"ab\" and t=\"a\" Expecting \"a\" Getting:" + minWindowLeetCode(s, t));
         t = "b";
-        System.out.println("Output of s = \"ab\" and t=\"b\" Expecting \"b\" Getting:" + minWindow(s, t));
+        System.out.println("Output of s = \"ab\" and t=\"b\" Expecting \"b\" Getting:" + minWindowLeetCode(s, t));
     }
 }
